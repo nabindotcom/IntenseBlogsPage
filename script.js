7a072f9200637bdabb926481a21804bb3bcc7382
Nabin Sunar <nabinsunar0102@gmail.com>
2025-01-04 07:46:36 +0000

    error fixed

--- script.js
+++ script.js
@@ -5,9 +5,5 @@ hamburger.addEventListener("click", () => {
 
   const menu = document.querySelector(".menu");
   menu.style.right = menu.style.right === "40px" ? "-140px" : "40px";
-  // if (menu.style.right === "40px") {
-  //   document.querySelector("#main").addEventListener("click", () => {
-  //     console.log("hi");
-  //   });
-  }
+  
 });
