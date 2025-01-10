e91e4dfe01ee02fb36f056987c93297a93a5b231
Nabin Sunar <nabinsunar0102@gmail.com>
2025-01-10 22:07:25 +0545

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ const hamburger = document.querySelector(".hamburger");
 hamburger.addEventListener("click", () => {
   console.log("clk");
 
-  const menu = document.querySelector(".menu");
-  menu.style.right = menu.style.right === "40px" ? "-140px" : "40px";
+  const menu = document.querySelector("nav");
+  menu.style.right = menu.style.right === "0px" ? "-280px" : "0px";
+});
+
+// language show hide
+const languageBtn = document.querySelector(".languageBtn");
+languageBtn.addEventListener("click", () => {
+  const dropdown = document.querySelector(".dropdown");
+  dropdown.style.display = dropdown.style.display === "flex" ? "none" : "flex";
 });
